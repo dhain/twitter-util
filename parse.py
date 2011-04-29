90fd79ad4de8eebe983f44506b8329e0c2cf1abe
David Zuwenden <dhain@zognot.org>
2011-04-29 12:25:55 -0700

    make parser more robust

--- parse.py
+++ parse.py
@@ -2,16 +2,36 @@
 """
 import sys
 import json
+import logging
+
+import util
 
 
 def parse(f):
+    log = logging.getLogger('parse')
     while True:
-        length = int(f.readline())
-        message = f.read(length)
-        yield json.loads(message)
+        data = f.readline()
+        if not data:
+            break
+        data = data.strip()
+        if not data:
+            continue
+        try:
+            length = int(data)
+        except ValueError:
+            log.debug('invalid message length: %r', data)
+            continue
+        data = f.read(length)
+        try:
+            obj = json.loads(data)
+        except ValueError:
+            log.debug('invalid json message: %r', data)
+            continue
+        yield obj
 
 
 if __name__ == '__main__':
+    util.initialize_logging()
     import pprint
     for data in parse(sys.stdin):
         pprint.pprint(data)
